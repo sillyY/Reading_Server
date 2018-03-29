d263cc02c943b0d9a31d9653c8f6817032264192
ly321 <965768042@qq.com>
2018-03-29 16:41:32 +0800

    add

--- routes/read.js
+++ routes/read.js
@@ -14,6 +14,19 @@ router.get('/cats', async (ctx, next) => {
         }
     }
 })
+//小说分类下小类别
+router.get('/cats/lv2', async (ctx, next) => {
+    try {
+        let url = "http://api.zhuishushenqi.com/cats/lv2",
+            data = await request.getData(url);
+        ctx.body = data
+    } catch (error) {
+        ctx.body = {
+            error: "发生错误",
+            detail: error
+        }
+    }
+})
 //小说分类列表
 router.get('/book/by-categories', async (ctx, next) => {
     try {
@@ -54,7 +67,7 @@ router.get('/rank', async (ctx, next) => {
     }
 })
 //小说排行榜小说
-router.get('/rank/:id', async(ctx, next) => {
+router.get('/rank/:id', async (ctx, next) => {
     try {
         let url = `http://api.zhuishushenqi.com${ctx.path}`,
             data = await request.getData(url);
@@ -66,4 +79,44 @@ router.get('/rank/:id', async(ctx, next) => {
         }
     }
 })
+//换源
+router.get('/toc', async (ctx, next) => {
+    try {
+        let url = `http://api.zhuishushenqi.com/toc?view=summary&${ctx.querystring}`,
+            data = await request.getData(url);
+        ctx.body = data
+    } catch (error) {
+        ctx.body = {
+            error: "发生错误",
+            detail: error
+        }
+    }
+})
+//小说目录
+router.get('/toc/:id', async (ctx, next) => {
+    try {
+        let url = `http://api.zhuishushenqi.com${ctx.path}?view=chapters`,
+            data = await request.getData(url);
+        ctx.body = data
+    } catch (error) {
+        ctx.body = {
+            error: "发生错误",
+            detail: error
+        }
+    }
+})
+//小说章节内容
+router.get('/chapter/:link', async (ctx, next) => {
+    try {
+        let url = `http://api.zhuishushenqi.com${ctx.path}?k=2124b73d7e2e1945&t=1468223717`,
+            data = await request.getData(url);
+        ctx.body = data
+    } catch (error) {
+        ctx.body = {
+            error: "发生错误",
+            detail: error
+        }
+    }
+})
+
 module.exports = router
